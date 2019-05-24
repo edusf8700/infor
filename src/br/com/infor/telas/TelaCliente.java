0975bb6027b9c074c6c7f8003014959ae838fe5d
Eduardo Freitas <edusf8700@gmail.com>
2019-05-24 02:36:38 -0300

    Vinculando cliente a tabela Os

--- src/br/com/infor/telas/TelaCliente.java
+++ src/br/com/infor/telas/TelaCliente.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -183,6 +183,9 @@ public class TelaCliente extends javax.swing.JInternalFrame {
         jLabel6 = new javax.swing.JLabel();
         txtCliId = new javax.swing.JTextField();
 
+        setClosable(true);
+        setIconifiable(true);
+        setMaximizable(true);
         setTitle("Clientes");
 
         jLabel1.setText("* Nome");
